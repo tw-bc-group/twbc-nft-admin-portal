8653f81eed952ed32e5322e15cf4091e90ee0c30
Yibing Tan <yibtan@thoughtworks.com>
2022-03-15 15:31:52 +0800

    chore: add react-router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Button } from 'antd';
+import NFTList from './routes/list'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <Button type="primary">NFT home page</Button>
-  </div>
+  <Router>
+    <div className="App">
+      <Button type="primary">NFT home page</Button>
+    </div>
+    <Routes>
+      <Route path="/" element={<NFTList/>} />
+    </Routes>
+  </Router>
 );
 
 export default App;
---
+++ src/routes/list.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NFTList = () => (
+  <div>list page</div>
+)
+
+export default NFTList
