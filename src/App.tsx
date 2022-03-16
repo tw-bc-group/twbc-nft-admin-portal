277ab2f1858efc7e54d31e22e7ee18dd5565ef94
Yibing Tan <yibtan@thoughtworks.com>
2022-03-16 10:47:01 +0800

    feat: add header to app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Layout } from 'antd';
 import NFTList from './routes/list'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+const { Header } = Layout
 
 const App = () => (
   <Router>
-    <div className="App">
-      <Button type="primary">NFT home page</Button>
-    </div>
+    <Header className="header">
+      <div className="header-left">
+        <div className="logo" />
+        <div className="portal">Portal</div>
+      </div>
+      <div className="user-name">Admin username</div>
+    </Header>
     <Routes>
       <Route path="/" element={<NFTList/>} />
     </Routes>
