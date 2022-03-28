e405c7b222e9efae78e56c23a72dee272e94cf47
xudongdong <dongdong.xu1@thoughtworks.com>
2022-03-28 16:39:01 +0800

    fix: use correct PRODUCTION_API_URL

--- src/utils/config/index.ts
+++ src/utils/config/index.ts
@@ -1,7 +1,7 @@
 import { get } from "lodash";
 
 const DEVELOPMENT_API_URL = "https://dev.api.nft.cn.blockchain.thoughtworks.cn/";
-const PRODUCTION_API_URL = "https://dev.nft.cn.blockchain.thoughtworks.cn/";
+const PRODUCTION_API_URL = "https://api.nft.cn.blockchain.thoughtworks.cn/";
 const environment = process.env.NODE_ENV || "development";
 
 const URL_MAP = {
